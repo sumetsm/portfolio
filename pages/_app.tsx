c49582136455464ca1f890214c7f7f43c046234a
sumetsm <sumetssr@gmail.com>
2023-01-16 09:59:36 +0700

    first-commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,12 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className='grid grid-cols-12 gap-6'>
+    <div className='col-span-3 bg-white'>
+      Sidebar
+    </div>
+    <div className='col-span-9 bg-white'>
+      <Component {...pageProps} />
+    </div>
+  </div>
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
